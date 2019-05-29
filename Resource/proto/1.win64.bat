6de4559b51e0a62c654cffc07154a3a24f41c0ff
lori227 <lori227@qq.com>
2019-05-29 12:54:34 +0800

    protobuf

--- Resource/proto/1.win64.bat
+++ Resource/proto/1.win64.bat
@@ -2,14 +2,19 @@
 
 for /f "delims=" %%a in ('dir /b/a-d/oN *.proto') do 3.protoc.exe -I=./ --cpp_out=dllexport_decl=LIBPROTOC_EXPORT:./ %%a
 
-set protopath=..\..\Server\KFContrib\KFProtocol
+set serverprotopath=..\..\Server\KFContrib\KFProtocol
+set clientpublicpath=..\..\UE4Client\Source\Fighter\Public\Protocol
+set clientprivatepath=..\..\UE4Client\Source\Fighter\Private\Protocol
+
 set currentpath=%cd%
 
-xcopy *.pb.* %protopath%\ /y
+xcopy *.pb.* %serverprotopath%\ /y
+xcopy *.pb.h %clientpublicpath%\ /y
+xcopy *.pb.cc %clientprivatepath%\ /y
 
 del *.pb.*
 
-cd %protopath%
+cd %serverprotopath%
 "%VS150COMNTOOLS%..\IDE\Devenv" KFProtocol.vcxproj /build "Debug|X64"  /project KFProtocol
 "%VS150COMNTOOLS%..\IDE\Devenv" KFProtocol.vcxproj /build "Release|X64" /project KFProtocol
 cd %currentpath%
---
+++ UE4Client/Source/Fighter/Public/Protocol/Protocol.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "Public/Protocol/FrameDefineMessage.pb.h"
+#include "Public/Protocol/FrameCodeMessage.pb.h"
+#include "Public/Protocol/FrameEnumMessage.pb.h"
+#include "Public/Protocol/FrameMessage.pb.h"
+#include "Public/Protocol/FrameClientMessage.pb.h"
+#include "Public/Protocol/FrameServerMessage.pb.h"
+
+#include "Public/Protocol/CodeMessage.pb.h"
+#include "Public/Protocol/EnumMessage.pb.h"
+#include "Public/Protocol/DefineMessage.pb.h"
+#include "Public/Protocol/ClientMessage.pb.h"
+#include "Public/Protocol/ServerMessage.pb.h"
+
+#include "google/protobuf/util/json_util.h"
