310bfae58df9afdd866482f194d9c5e38fcfda10
lori227 <lori227@qq.com>
2019-05-31 15:03:41 +0800

    libproto

--- UE4Client/Plugins/libprotobuf/ThirdParty/include/cmake/vsbuild/build150-protobuf-x64.bat
+++
@@ -1,9 +0,0 @@
-
-:  build protobuf
-
-"%VS150COMNTOOLS%..\IDE\Devenv" protobuf.sln /build "Debug|X64" /project libprotobuf
-copy /y .\Debug\libprotobufd.lib ..\..\..\..\..\_lib\win64\debug\3rd\
-
-
-"%VS150COMNTOOLS%..\IDE\Devenv" protobuf.sln /build "Release|X64" /project libprotobuf
-copy /y .\Release\libprotobuf.lib ..\..\..\..\..\_lib\win64\release\3rd\
