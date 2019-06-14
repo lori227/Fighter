5125e2bf58ab547588b056dc4f61121ecce415b2
lori227 <lori227@qq.com>
2019-06-15 00:36:06 +0800

    Update make_version.sh

--- Server/_build/linux/make_version.sh
+++ Server/_build/linux/make_version.sh
@@ -53,6 +53,6 @@ cp -rf ../Resource/script/ _bin/
 
 cd _bin/_gcm/builder/
 chmod 777 gcm_build
-./gcm_build -p "chess" -s $svnversion -b $2 -c $1 -m $3 -v $version -n 1.2 -t $uptype
+./gcm_build -p "fighter" -s $svnversion -b $2 -c $1 -m $3 -v $version -n 1.2 -t $uptype
 
 cd ../../../../../
