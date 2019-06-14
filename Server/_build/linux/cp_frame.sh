238200b45759c6d7454f53f82a0ebabeaffca210
lori227 <lori227@qq.com>
2019-06-14 21:54:10 +0800

    Update cp_frame.sh

--- Server/_build/linux/cp_frame.sh
+++ Server/_build/linux/cp_frame.sh
@@ -9,7 +9,7 @@ mkdir -p $libpath/3rd
 rm -rf $libpath/$1
 mkdir -p $libpath/$1
 
-framepath=../../../../../../frame/trunk
+framepath=../../../../KFrame
 framelibpath=$framepath/_lib/linux
 cp -rf $framelibpath/3rd/* $libpath/3rd
 cp -rf $framelibpath/$1/* $libpath/$1
