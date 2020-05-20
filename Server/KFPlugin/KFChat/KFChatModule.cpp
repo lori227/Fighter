0f3dccdf40aa4d32612320fa0e859defb3b676cc
lori227 <lori227@qq.com>
2020-05-20 17:29:35 +0800

    add chat pulgin

---
+++ Server/KFPlugin/KFChat/KFChatModule.cpp
@@ -0,0 +1,16 @@
+﻿#include "KFChatModule.hpp"
+
+namespace KFrame
+{
+    void KFChatModule::BeforeRun()
+    {
+        //__REGISTER_MESSAGE__( KFMsg::MSG_FOOT_USE_REQ, &KFHeroModule::HandleFootUseReq );
+    }
+
+    void KFChatModule::BeforeShut()
+    {
+        //__UN_MESSAGE__( KFMsg::MSG_FOOT_USE_REQ );
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
