28a4d7452a517871ee4a0c9c19c79bb5abb53bc1
lori227 <lori227@qq.com>
2020-11-03 18:33:05 +0800

    玩家属性

--- Server/KFInterface/KFReset/KFResetInterface.h
+++ Server/KFInterface/KFReset/KFResetInterface.h
@@ -5,15 +5,16 @@
 
 namespace KFrame
 {
-    typedef std::function< void( KFEntity*, const KFTimeData*, uint64, uint64 ) > KFResetFunction;
+    typedef std::function< void( KFEntity*, uint32, const KFTimeData*, uint64, uint64 ) > KFResetFunction;
     class KFResetInterface : public KFModule
     {
     public:
         // 注册重置逻辑
         template< class T >
-        void RegisterResetFunction( uint32 timeid, uint32 count, T* object, void ( T::*handle )( KFEntity*, const KFTimeData*, uint64, uint64 ) )
+        void RegisterResetFunction( uint32 timeid, uint32 count, T* object, void ( T::*handle )( KFEntity*, uint32, const KFTimeData*, uint64, uint64 ) )
         {
-            KFResetFunction function = std::bind( handle, object, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4 );
+            KFResetFunction function = std::bind( handle, object, std::placeholders::_1, std::placeholders::_2,
+                                                  std::placeholders::_3, std::placeholders::_4, std::placeholders::_5 );
             AddResetFunction( timeid, count, typeid( T ).name(), function );
         }
 
@@ -44,7 +45,7 @@ namespace KFrame
     __KF_INTERFACE__( _kf_reset, KFResetInterface );
     //////////////////////////////////////////////////////////////////////////////////////
 
-#define __KF_RESET_FUNCTION__( function ) void function( KFEntity* player, const KFTimeData* timedata, uint64 lasttime, uint64 nowtime )
+#define __KF_RESET_FUNCTION__( function ) void function( KFEntity* player, uint32 timeid, const KFTimeData* timedata, uint64 lasttime, uint64 nowtime )
 #define __REGISTER_RESET__( timeid, function ) _kf_reset->RegisterResetFunction( timeid, 1u, this, function )
 #define __REGISTER_RESET_COUNT__( timeid, count, function ) _kf_reset->RegisterResetFunction( timeid, count, this, function )
 #define __UN_RESET__() _kf_reset->UnRegisterResetFunction( this )
