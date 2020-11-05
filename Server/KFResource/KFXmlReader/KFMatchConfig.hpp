2401b56d48997ca7d74a223541c9137364d25f60
lori227 <lori227@qq.com>
2020-11-05 18:28:12 +0800

    config

---
+++ Server/KFResource/KFXmlReader/KFMatchConfig.hpp
@@ -0,0 +1,50 @@
+﻿#ifndef __KF_MATCH_CONFIG_H__
+#define __KF_MATCH_CONFIG_H__
+
+#include "KFrame.h"
+#include "KFConfig.h"
+
+namespace KFrame
+{
+    class KFMatchSetting : public KFIntSetting
+    {
+    public:
+        // 最大玩家数量
+        uint32 _max_count = 0u;
+
+        // 上限分差
+        uint32 _upper_grade = 0u;
+
+        // 下限分差
+        uint32 _lower_grade = 0u;
+
+        // 添加机器人时间间隔
+        uint32 _add_robot_time = 0u;
+
+        // 添加机器人数量
+        uint32 _add_robot_count = 0u;
+    };
+    ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    class KFMatchConfig : public KFConfigT< KFMatchSetting >, public KFInstance< KFMatchConfig >
+    {
+    public:
+        KFMatchConfig()
+        {
+            _file_name = "match";
+        }
+
+    protected:
+        // 读取配置
+        void ReadSetting( KFNode& xmlnode, KFMatchSetting* kfsetting )
+        {
+            kfsetting->_max_count = xmlnode.GetUInt32( "MaxCount" );
+            kfsetting->_upper_grade = xmlnode.GetUInt32( "UpperGrade" );
+            kfsetting->_lower_grade = xmlnode.GetUInt32( "LowerGrade" );
+            kfsetting->_add_robot_time = xmlnode.GetUInt32( "AddRobotTime" );
+            kfsetting->_add_robot_count = xmlnode.GetUInt32( "AddRobotCount" );
+        }
+    };
+}
+
+#endif
---
+++ Server/KFResource/KFXmlReader/KFNameConfig.hpp
@@ -0,0 +1,56 @@
+﻿#ifndef __KF_NAME_CONFIG_H__
+#define __KF_NAME_CONFIG_H__
+
+#include "KFrame.h"
+#include "KFConfig.h"
+
+namespace KFrame
+{
+    class KFNameSetting : public KFIntSetting
+    {
+    public:
+        // 名字列表
+        StringVector _name_list;
+    };
+    ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    class KFNameConfig : public KFConfigT< KFNameSetting >, public KFInstance< KFNameConfig >
+    {
+    public:
+        KFNameConfig()
+        {
+            _file_name = "name";
+        }
+
+        // 随机名字
+        const std::string& RandName() const
+        {
+            static std::string _name = "";
+            _name.clear();
+
+            for ( auto iter : _settings._objects )
+            {
+                auto kfsetting = iter.second;
+                if ( kfsetting->_name_list.empty() )
+                {
+                    continue;
+                }
+
+                auto index = KFGlobal::Instance()->RandRatio( ( uint32 )kfsetting->_name_list.size() );
+                _name += kfsetting->_name_list[ index ];
+            }
+
+            return _name;
+        }
+
+    protected:
+        // 读取配置
+        void ReadSetting( KFNode& xmlnode, KFNameSetting* kfsetting )
+        {
+            auto name = xmlnode.GetString( "Name" );
+            kfsetting->_name_list.push_back( name );
+        }
+    };
+}
+
+#endif
